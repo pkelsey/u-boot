1e17f051d8d16b993881d84f2dcf4c2d73ffd13b
Patrick Kelsey <kelsey@ieee.org>
2014-03-05 08:40:46 -0500

    Applied current crochet-freebsd BeagleBone patches to branch v2013.04-crochet-bbb

--- config.mk
+++ config.mk
@@ -95,7 +95,7 @@ HOSTCC		 = $(call os_x_before, 10, 5, "cc", "gcc")
 HOSTCFLAGS	+= $(call os_x_before, 10, 4, "-traditional-cpp")
 HOSTLDFLAGS	+= $(call os_x_before, 10, 5, "-multiply_defined suppress")
 else
-HOSTCC		= gcc
+HOSTCC		?= gcc
 endif
 
 ifeq ($(HOSTOS),cygwin)
